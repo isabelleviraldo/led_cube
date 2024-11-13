9c2a06f7de6887a089e7df4bfc84ffe6bad7cf62
Isabelle Viraldo <108153804+isabelleviraldo@users.noreply.github.com>
2024-11-12 21:21:53 -0800

    Update and rename image_lit_on_matrix.ino to image_on_matrix.ino

---
+++ tests/image_on_matrix.ino
@@ -0,0 +1,86 @@
+//shift registers 1 and 2
+int latchPin = 3;
+int clockPin = 18;
+int dataPin = 19;
+int latchPin2 = 4;
+int clockPin2 = 15;
+int dataPin2 = 16;
+
+int numOfRows = 6;
+int numOfCols = 8;
+
+byte on = 0xff;
+byte off = 0x00;
+
+int place = 0;
+
+int rowControl[6][8] = { { 1, 0, 0, 0, 0, 0, 0, 0 },
+                         { 0, 1, 0, 0, 0, 0, 0, 0 },
+                         { 0, 0, 1, 0, 0, 0, 0, 0 },
+                         { 0, 0, 0, 1, 0, 0, 0, 0 },
+                         { 0, 0, 0, 0, 1, 0, 0, 0 },
+                         { 0, 0, 0, 0, 0, 1, 0, 0 } };
+
+int image[6][8] = { { 0, 0, 1, 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0, 1, 0, 0 },
+                    { 0, 0, 1, 0, 0, 1, 0, 0 },
+                    { 0, 0, 0, 0, 0, 0, 0, 0 },
+                    { 1, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 1, 1, 1, 1, 1, 1, 1 } };
+
+void setup() {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(latchPin2, OUTPUT);
+  pinMode(clockPin2, OUTPUT);
+  pinMode(dataPin2, OUTPUT);
+}
+
+void loop() {
+  displayImage();
+}
+
+void displayImage(){
+  for (int r = 0; r <= numOfRows; r++) {
+    //controls the rows
+    for (int c = 0; c <= numOfRows; c++) {
+      digitalWrite(latchPin2, LOW);
+      if (rowControl[r][c] == 1) {
+        shiftOut(dataPin2, clockPin2, MSBFIRST, on);
+      } else {
+        shiftOut(dataPin2, clockPin2, MSBFIRST, off);
+      }
+      digitalWrite(latchPin2, HIGH);
+    }
+    //controls the cols
+    for (int c = 0; c <= numOfCols; c++) {
+      digitalWrite(latchPin, LOW);
+      if (image[r][c] == 1) {
+        shiftOut(dataPin, clockPin, MSBFIRST, off);
+      } else {
+        shiftOut(dataPin, clockPin, MSBFIRST, on);
+      }
+      digitalWrite(latchPin, HIGH);
+    }
+    
+    delay(2);
+  }
+}
+
+void clear(){
+  for (int r = 0; r <= numOfRows; r++) {
+    //controls the rows
+    for (int c = 0; c <= numOfRows; c++) {
+      digitalWrite(latchPin2, LOW);
+      shiftOut(dataPin2, clockPin2, MSBFIRST, off);
+      digitalWrite(latchPin2, HIGH);
+    }
+    for (int c = 0; c <= numOfCols; c++) {
+      digitalWrite(latchPin, LOW);
+      shiftOut(dataPin, clockPin, MSBFIRST, on);
+      digitalWrite(latchPin, HIGH);
+    }
+  }
+}
